b3037831eedca838a1477798c195314b8970a527
riolly <alberto.cahyantara@gmail.com>
2025-05-31 19:50:05 +0700

    add tanstack query

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,6 +7,10 @@ export default function Header() {
         <div className="px-2 font-bold">
           <Link to="/">Home</Link>
         </div>
+
+        <div className="px-2 font-bold">
+          <Link to="/demo/tanstack-query">TanStack Query</Link>
+        </div>
       </nav>
     </header>
   )
